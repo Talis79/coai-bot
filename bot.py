984e62cac11480c3a5c6954d6a79a9172c4c4d60
Talis79 <thomas.michael@tutamail.com>
2025-07-23 03:50:55 +0200

    final

--- bot.py
+++ bot.py
@@ -90,10 +90,11 @@ async def main():
 
 
 if __name__ == '__main__':
-    import nest_asyncio
+import nest_asyncio
 import asyncio
 
 nest_asyncio.apply()
 loop = asyncio.get_event_loop()
 loop.create_task(main())
 loop.run_forever()
+
